1dbc149f1a949807f81cd54d8974fdaa2fc9fa38
miraj <miraj00@gmail.com>
2021-07-31 19:54:42 -0400

    updated as of 7-31-21 7:50pm

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,19 +11,6 @@ $(document).ready(function () {
 
   $.backstretch("./assets/images/pic2.JPEG");
 
-/*
-  function capture() {
-  html2canvas(document.body, {
-    onrendered: function(canvas)
-    {
-    var img = canvas.toDataURL();
-    $("#result-image").attr('src', img).show();
-    }
-    });
-  }
-
-*/
-
 
 var optionsList = document.querySelector("#mySelection");
 
